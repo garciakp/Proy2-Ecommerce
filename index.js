be43dbfe9ed2ebf144fceb921e8ceb16318758a1
Samuel López <55517791+smlpj@users.noreply.github.com>
2022-11-20 11:41:01 -0500

    @feature Category route dadded

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-require('dotenv').config();
-const cors = require('cors')
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+const cors = require("cors");
 
-const usersRoutes = require('./routes/userRoutes')
-const postsRoutes = require('./routes/productRoutes')
-const cartRoutes = require('./routes/cartRoutes')
-const reviewsRoutes = require('./routes/reviewRoutes')
-const historyRoutes = require('./routes/historyRoutes')
+const usersRoutes = require("./routes/userRoutes");
+const postsRoutes = require("./routes/productRoutes");
+const cartRoutes = require("./routes/cartRoutes");
+const reviewsRoutes = require("./routes/reviewRoutes");
+const historyRoutes = require("./routes/historyRoutes");
+const categoryRoutes = require("./routes/categoryRoutes");
 
 app.use(bodyParser.json());
 
 //Connect to DB
-mongoose.connect(
-    process.env.DB_CONNECTION,
-    { useNewUrlParser: true},
-    () => console.log('Connected to DB')
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log("Connected to DB")
 );
 
 //Routes
-app.get('/', (req, res) => {
-    res.send('We are on home');
+app.get("/", (req, res) => {
+  res.send("We are on home");
 });
-app.use(express.json())
-app.use(cors())
-app.use('/users', usersRoutes)
-app.use('/products', postsRoutes)
-app.use('/cart', cartRoutes)
-app.use('/reviews', reviewsRoutes)
-app.use('/history', historyRoutes)
+app.use(express.json());
+app.use(cors());
+app.use("/users", usersRoutes);
+app.use("/products", postsRoutes);
+app.use("/cart", cartRoutes);
+app.use("/reviews", reviewsRoutes);
+app.use("/history", historyRoutes);
+app.use("/category", categoryRoutes);
 
 //Listen to server
 const port = process.env.PORT || 3000;
---
+++ routes/categoryRoutes.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const router = express.Router();
+
+const productSchema = require("../models/ProductModel");
+
+//Create category
+router.post("/", async (req, res) => {
+  const category = new productSchema({
+    category: req.body.category,
+  });
+  try {
+    const savedCategory = await category.save();
+    res.json(savedCategory);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//Get all categories
+router.get("/", async (req, res) => {
+  try {
+    const categories = await productSchema.find();
+    res.json(categories);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//Get product by category
+router.get("/:category", async (req, res) => {
+  try {
+    const category = await product
+      .find({ category: req.params.category })
+      .populate("category");
+    res.json(category);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//update category
+router.patch("/:category", async (req, res) => {
+  try {
+    const updatedCategory = await productSchema.updateOne(
+      { category: req.params.category },
+      { $set: { category: req.body.category } }
+    );
+    res.json(updatedCategory);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+//Delete category
+router.delete("/:category", async (req, res) => {
+  try {
+    const removedCategory = await productSchema.remove({
+      category: req.params.category,
+    });
+    res.json(removedCategory);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
+module.exports = router;
